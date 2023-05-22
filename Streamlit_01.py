023b5694d5d8ee322e34990b24d7c02d20870ef4
AGLajoie <60831718+AGLajoie@users.noreply.github.com>
2023-05-22 19:55:52 -0400

    Add files via upload

---
+++ Streamlit_01.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+       @@@@@                           
+      @%   %@  @      %@@@%%     %@     
+     @%       @%     @%    %%   @@      
+    @%       @ @    @@         @@       
+   @%       @  @   %@         @@        
+  @%       @%@%@%@%@%@%@%@%@%@%@%@%@     
+  %%     @%    @  %@      %@ @@     
+  %@   @%      @  %@%  %@%  %@%      
+   %@@@%       %@  %@@@%    %@@@@%    
+                
+@author: AGL
+"""
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame(
+   np.random.randn(50, 20),
+   columns=('col %d' % i for i in range(20)))
+
+st.dataframe(df)  # Same as st.write(df)
