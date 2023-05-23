614092c205709d04d536d23fba148da805af2277
AGLajoie <60831718+AGLajoie@users.noreply.github.com>
2023-05-22 21:12:23 -0400

    Update Streamlit_01.py

--- Streamlit_01.py
+++ Streamlit_01.py
@@ -16,6 +16,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import streamlit as st
+import streamlit_gsheets
 from streamlit_gsheets import GSheetsConnection
 
 url = "https://docs.google.com/spreadsheets/d/1Z2n8Hl8M_TzJx1-hun4FXbGJjvcupC6q89ijQbLmYgA/edit?usp=sharing"
